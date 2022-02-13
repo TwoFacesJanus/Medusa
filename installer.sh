e4d360a13b237f837bf6d98c2a865f16e51d5bc7
Janus <99186974+TwoFacesJanus@users.noreply.github.com>
2022-02-13 11:26:17 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,69 +1,88 @@
 #!/bin/bash
 set -e
 
-ARCH_PACKAGES="python python-pip git"
-DEB_PACKAGES="python3 python3-pip git"
-CENT_PACKAGES="python3 python3-pip git"
+PACKAGES=NONE
+GIT_REPOSITORY="https://github.com/TwoFacesJanus/Medusa.git"
+DEPENDENCIES="google-cloud-dialogflow\n  |               configparser\n  |               pyttsx3\n"
 GREEN='\033[1;32m'
 NORMAL='\033[0m'
 
+DISTRO=NONE
 clear
 
-echo -en "==============$GREEN MEDUSA $NORMAL==============\n"
+function banner(){
+    echo -en "<========$GREEN MEDUSA $NORMAL========>\n"
+    echo -en "  | Author: TwoFacesJanus\n"
+    echo -en "  | Github: https://github.com/TwoFacesJanus/Medusa\n"
+    echo -en "  | License: MIT\n"
+    echo -en "  | -------------------------\n" 
+}
 
-if cat /etc/*release | grep ^NAME | grep CentOS &>/dev/null; then
-    echo -en "Packages: $CENT_PACKAGES"
-    echo -en "\n==================================\n"
-    echo -en "[ INFO ]=> Installing packages..." 
 
-elif cat /etc/*release | grep ^NAME | grep Garuda &>/dev/null; then
-    echo -en "Packages: $ARCH_PACKAGES"
-    echo -en "\n==================================\n"
-    echo -en "[ INFO ]=> Installing packages..." 
-    sudo pacman -Sy $ARCH_PACKAGES --noconfirm &>/dev/null
+
+
+function CurrentDistro(){
+
+    if cat /etc/*release | grep ^NAME | grep CentOS &>/dev/null; then
+        DISTRO="cent"
+        PACKAGES="python3 python3-pip git"
+
+    elif cat /etc/*release | grep ^NAME | grep Garuda &>/dev/null; then
+        DISTRO="arch"
+        PACKAGES="python python-pip git"
+        
+    elif cat /etc/*release | grep ^NAME | grep Arch &>/dev/null; then
+        DISTRO="arch"
+        PACKAGES="python python-pip git"
+
+    elif cat /etc/*release | grep ^NAME | grep Debian &>/dev/null; then
+        DISTRO="debian"
+        PACKAGES="python3 python3-pip git"
+
+    elif cat /etc/*release | grep ^NAME | grep Ubuntu &>/dev/null; then
+        DISTRO="debian"
+        PACKAGES="python3 python3-pip git"
+
+    elif cat /etc/*release | grep ^NAME | grep Fedora &>/dev/null; then
+        DISTRO="cent"
+        PACKAGES="python3 python3-pip git"
+
+
+    else 
+        echo "Whoops..."
+        exit 1;
+    fi
+}
+
+banner
+CurrentDistro
+
+echo "  | Distro: $DISTRO" 
+echo "  | Packages: $PACKAGES"
+echo -en "  | Dependencies: $DEPENDENCIES"
+echo "  |"
+echo -en "  | [$GREEN INFO $NORMAL]=> Installing packages...\n"
+echo -en "  | "
+
+case $DISTRO in
+
+    "cent")
+        echo "cent"
+        sudo dnf update -y $PACKAGES &>/dev/null
+        ;;
     
+    "arch")
+        sudo pacman -Sy $PACKAGES --noconfirm &>/dev/null
+        ;;
+esac
 
-elif cat /etc/*release | grep ^NAME | grep Arch &>/dev/null; then
-    echo -en "Packages: $ARCH_PACKAGES"
-    echo -en "\n==================================\n"
-    echo -en "[ INFO ]=> Installing packages..." 
-    sudo pacman -Sy $ARCH_PACKAGES --noconfirm &>/dev/null
+echo -en "\n  | [$GREEN INFO $NORMAL]=> Downloading repository..."
+git clone $GIT_REPOSITORY &>/dev/null
 
-
-elif cat /etc/*release | grep ^NAME | grep Debian &>/dev/null; then
-    echo -en "Packages: $DEB_PACKAGES"
-    echo -en "\n==================================\n"
-    echo -en "[ \033[32mINFO ]=> Installing packages..." s
-    udo apt-get update -y
-    sudo apt-get install $DEB_PACKAGES -y &>/dev/null
-
-elif cat /etc/*release | grep ^NAME | grep Ubuntu &>/dev/null; then
-    echo -en "Packages: $DEB_PACKAGES"
-    echo -en "\n=================================\n"
-    echo -en "[ INFO ]=> Installing packages..." 
-    sudo apt-get update -y &>/dev/null
-    sudo apt-get install $DEB_PACKAGES -y &>/dev/null
-
-elif cat /etc/*release | grep ^NAME | grep Fedora &>/dev/null; then
-    echo -en "Packages: $CENT_PACKAGES"
-    echo -en "\n==================================\n"
-    echo -en "[ INFO ]=> Installing packages..." 
-    sudo dnf update -y &>/dev/null
-
-else 
-    echo "Whoops..."
-    exit 1;
-fi
-
-echo -en "\n[$GREEN INFO $NORMAL]=> Done.\n"
-
-echo -en "[ INFO ]=> Downloading repository...\n"
-git clone https://github.com/TwoFacesJanus/Medusa.git &>/dev/null
-echo -en "[$GREEN INFO $NORMAL]=> Done.\n"
-
-echo -en "[ INFO ]=> Installing requirements...\n"
+echo -en "\n  | [$GREEN INFO $NORMAL]=> Installing Dependencies..."
 pip3 install -r Medusa/requirements.txt &>/dev/null
-echo -en "[$GREEN INFO $NORMAL]=> Done.\n"
+echo -en "\n  | [$GREEN INFO $NORMAL]=> Done\n"
+echo -en "<=========================>\n"
 
 
-exit 0
+
