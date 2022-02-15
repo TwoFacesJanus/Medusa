100f0a70821c35a002f1e0bcbd4c0bab52ee42c2
Janus <99186974+TwoFacesJanus@users.noreply.github.com>
2022-02-15 05:10:15 +0000

    Update flower.py

--- src/flower.py
+++ src/flower.py
@@ -4,6 +4,7 @@ import pyttsx3
 import json
 import random
 
+
 class Prepare:
     def __init__(self):
         with open("configuration/local_intents.json") as jsonfile:
@@ -11,9 +12,7 @@ class Prepare:
             jsonfile.close()
         config = ConfigParser()
         config.read("configuration/config.ini")
-        
         self.lang_code = config['DIALOGFLOW']['lang_code']
-    
 
     def search_in_intents(self, message):
         self.time = self.local_intents['intents'][0]['time'][self.lang_code]
@@ -21,11 +20,11 @@ class Prepare:
 
         if message in self.time:
             return "Time Now is 17:00"
-        
+
         elif message in self.weather:
             print("weather found!")
             return "Current Weather is cloudy"
-        
+
         else:
             print("not found!")
             return "unknown"
@@ -38,7 +37,6 @@ class Intent:
         self.session_client = dialogflow.SessionsClient()
         self.session = self.session_client.session_path(self.project_id, self.session_id)
 
-    
     def configure(self):
         config = ConfigParser()
         config.read("configuration/config.ini")
@@ -47,7 +45,6 @@ class Intent:
         self.session_id = config['DIALOGFLOW']['session_id']
         self.language_code = config['DIALOGFLOW']['lang_code']
 
-    
     def send_message(self, message):
         text_input = dialogflow.TextInput(text=message, language_code=self.language_code)
         query_input = dialogflow.QueryInput(text=text_input)
@@ -56,7 +53,6 @@ class Intent:
             request={"session": self.session, "query_input": query_input}
         )
 
-        
         print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
         return str(response.query_result.fulfillment_text)
 
@@ -65,12 +61,10 @@ class TTSX:
     def __init__(self):
         self.tts = pyttsx3.init()
         self.tts.setProperty('voice', 'en')
-    
 
     def say(self, message):
         self.tts.say(message)
         self.tts.runAndWait()
-
 
 intent = Intent()
 ttsx = TTSX()
