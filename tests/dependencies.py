9e6b9170af1cf6c78d0f4bbad5c5c85deccfe40f
Janus <99186974+TwoFacesJanus@users.noreply.github.com>
2022-02-15 07:37:31 +0000

    Update dependencies.py

--- tests/dependencies.py
+++ tests/dependencies.py
@@ -2,5 +2,6 @@ try:
     from google.cloud import dialogflow
     from configparser import ConfigParser
     import pyttsx3
+    from datetime import datetime
 except Exception as ex:
     print("[ INFO ]=> Error in dependencies.py library")
